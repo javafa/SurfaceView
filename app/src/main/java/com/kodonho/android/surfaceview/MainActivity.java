749fdd658e6d2e25c5a703cab1c1e710798b8bc8
javafa <nigne2016@gmail.com>
2016-10-20 17:13:16 +0900

    최초등록

---
+++ app/src/main/java/com/kodonho/android/surfaceview/MainActivity.java
@@ -0,0 +1,104 @@
+package com.kodonho.android.surfaceview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+    int deviceWidth = 0;
+    int deviceHeight = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        deviceHeight = metrics.heightPixels;
+        deviceWidth = metrics.widthPixels;
+
+        CustomSurfaceView surfaceView = new CustomSurfaceView(this);
+        setContentView(surfaceView);
+    }
+
+    public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+
+        private SurfaceThread thread;
+
+        public CustomSurfaceView(Context context) {
+            super(context);
+            getHolder().addCallback(this);
+            thread = new SurfaceThread(getHolder());
+            thread.setDaemon(true);
+        }
+
+        public CustomSurfaceView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public CustomSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+        }
+
+        // 뷰가 화면에 보여지는 시점
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            thread.start();
+        }
+
+        // 뷰에 변경사항 (사이즈 변경 등...)
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        // 화면에서 뷰가 보이지 않는 시점
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            thread.interrupt();
+        }
+    }
+
+    // 무한반복하면서 위에서 정의한 surface 뷰에 그림을 그려주는 역할을 한다
+    public class SurfaceThread extends Thread{
+        private SurfaceHolder surfaceHolder;
+        Paint paint = new Paint();
+        int x = 0;
+        int y = 0;
+
+        public SurfaceThread(SurfaceHolder holder){
+            // surface뷰에서 넘겨준 홀더를 가지고 작업을 한다
+            surfaceHolder = holder;
+        }
+
+        @Override
+        public void run(){
+            // 무한 반복하면서 그림을 그려준다
+            while(true){
+                // 그림을 그릴 캔버스를 가져오고
+                Canvas canvas = surfaceHolder.lockCanvas(null);
+                // 그림을 그릴 홀더를 동기화 한다
+                synchronized (surfaceHolder){
+                    paint.setColor(Color.WHITE);
+                    canvas.drawRect(0, 0, deviceWidth, deviceHeight,paint);
+                    paint.setColor(Color.BLUE);
+                    canvas.drawRect(x, y, x+50, y+50,paint);
+                }
+
+                x++; y++;
+                if(x > deviceWidth){
+                    x=0; y =0;
+                }
+                // 캔버스의 락을 해제하면 실제 뷰에 그림을 그리게 된다
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+}
